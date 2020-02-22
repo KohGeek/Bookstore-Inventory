52540348e5a041611a44cc4ca4d24e6c2fed9933
Koh Jun Dong <KohComputer@gmail.com>
2020-02-23 03:58:13 +0800

    Added a non-console build file
    - Linker settings.

---
+++ build - noconsole.bat
@@ -0,0 +1,10 @@
+@echo off
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+) else (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+)
+set compilerflags=/Od /Zi /EHsc /std:c++latest /I include
+set linkerflags=/OUT:bin\main.exe lib/nana.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib /SUBSYSTEM:windows /ENTRY:mainCRTStartup
+cl.exe %compilerflags% src\*.cpp /link %linkerflags%
+del bin\*.ilk *.obj *.pdb

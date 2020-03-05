5f864ed1561e970f2b0b6cee6eb47b068cec0297
TKF486 <60849963+TKF486@users.noreply.github.com>
2020-03-05 18:01:15 +0800

    creating layout
    simple layout

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -4,39 +4,19 @@
 #include "nana/gui/widgets/label.hpp"
 #include "nana/gui/filebox.hpp"
 #include "nana/gui/widgets/button.hpp"
-#include <nana/gui/widgets/textbox.hpp>
-#include <nana/gui/place.hpp>
+#include "nana/gui/widgets/textbox.hpp"
+#include "nana/gui/place.hpp"
+#include "nana/gui/compact.hpp"
+#include "nana/gui/widgets/button.hpp"
 
+void GUI()
+{
+	using namespace nana;
 
+	form menu;
+	
+    button butt1(menu, rectangle(10, 10, 10 , 10), true);
 
-void GUI(void){
-     using namespace nana;
-
-    form fm;
-
-    fm.events().click([]{
-        //It will get called, but it is not the first event handler to be called.
-    });
-
-    fm.events().click([](const arg_mouse& arg){
-        arg.stop_propagation();
-    });
-
-    fm.events().click([]{
-        //It will not get called.
-    });
-
-    fm.events().click.connect_unignorable([]{
-        //It will get called because it is unignorable.
-    });
-
-    fm.events().click.connect_front([]{
-        //It will get called firstly, because it is the beginning of the chain.
-    });
-
-    fm.show();
+    menu.show();
     exec();
-
-        fm.show();
-        exec();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,55 @@
+/**
+ *  \file calculator.cpp
+ *  \brief Nana Calculator
+ *	Nana 1.3 and C++11 is required.
+ *	This is a demo for Nana C++ Library.
+ *	It creates an intermediate level graphical calculator with few code.
+ */
+
+#include <nana/gui.hpp>
+#include <nana/gui/widgets/button.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include <nana/gui/place.hpp>
+#include <nana/gui/widgets/textbox.hpp>
+#include <forward_list>
+#include <map>
+#include <cassert>
+
+//#include <nana/paint/graphics.hpp"
+
 #include <iostream>
-#include <iomanip>
-#include <conio.h>
-#include "GUI.hpp"
+#include <chrono>
+
+#include <thread>
+
+using namespace nana;
+
+
+
 
 int main()
 {
-    using std::cout;
-    using std::cin;
-    using std::endl;
+       
+    
+    form fm;
+    fm.caption(("Test"));
+    textbox t1 {fm},
+            t2 {fm};
+    button bt1(fm,"Hello");
+      button bt2(fm,"Hello");
 
-        
+      t1.tip_string("Username:").multi_lines(false);
+      t2.tip_string("Password:").multi_lines(false).mask('*');
+      
+    place plc(fm);
+    plc.div("   <> <weight=80% vertical<><weight=70% vertical <vertical gap =10 textboxes arrange = [25,25] ><weight=25% gap = 5 buttons>        ><>     > <>");
+    plc.field("textboxes") << t1 << t2;
+    plc.field("buttons") << bt1 << bt2;
 
-    GUI();
+    plc.collocate();
+    
+    fm.show();
+    exec();
+    
 
-    cout << "Hello Easy C++ project!" << endl;
-
-    cout << "Press any key to continue...";
-    getch();
-    return 0;
 }

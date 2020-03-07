888f0899626fcd63e8088e28cd72a5dd99af4cd1
TKF486 <60849963+TKF486@users.noreply.github.com>
2020-03-08 00:20:05 +0800

    MENU BAR
    WITH BUTTON ONLY

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,4 @@
-/**
- *  \file calculator.cpp
- *  \brief Nana Calculator
- *	Nana 1.3 and C++11 is required.
- *	This is a demo for Nana C++ Library.
- *	It creates an intermediate level graphical calculator with few code.
- */
+
 
 #include <nana/gui.hpp>
 #include <nana/gui/widgets/button.hpp>
@@ -13,6 +7,8 @@
 #include <nana/gui/widgets/textbox.hpp>
 #include <nana/gui/widgets/tabbar.hpp>
 #include <nana/gui/widgets/group.hpp>
+#include <nana/gui/widgets/menu.hpp>
+#include <nana/gui/widgets/widget.hpp>
 
 #include <forward_list>
 #include <map>
@@ -28,53 +24,42 @@
 using namespace nana;
 
 
-
-
+void on_menu_item(menu::item_proxy& ip)
+{
+    std::size_t index = ip.index();
+    std::cout << "Menu item index:" << index <<
+    std::endl;
+}
 
 int main()
 {
        
     
    form fm;
-   fm.caption("Nana.exe");
-   place plc(fm);
-   bool really_quick{false};
-   label hello{fm, "Hello World"};
-   button btn {fm, "Quit"};
-   btn.tooltip("I will ask first");
-   group act(fm,"Actions");
 
-   act.add_option("Quick quickly")
-   .events().click([&] () {
-       really_quick = true;
-       });
-
-        act.add_option("Ask first")
-   .events().click([&] () {
-       really_quick = false;
-        });
-
-        btn.events().click([&]() {
-            if (!really_quick)
-            {
-                msgbox m(fm, "Our demo", msgbox::yes_no);
-                m.icon(m.icon_question);
-                m <<"Are you sure you want to quit";
-                auto response = m();
-                if (response != m.pick_yes)return;
-
-                
-            
-        }
-        API::exit();
-        });
    
+   fm.caption("Nana.exe");
+   button bt0{fm,"Add"};
+    button bt1{fm,"Delete"};
+     button bt2{fm, "Edit"};
+     textbox search{fm};
+    
+    
+
+    
+
+    
+
+			
+      
+    fm.show();
+    place plc(fm);
+    plc.div("vertical <weight = 20% < vertical  <weight = 20% < buttons arrange = [100,100,100,100]>  <search_box margin = [0,0,0,300]>  > <> > <> > <> <>   ");
+   
+    plc.field("buttons") << bt0 << bt1 << bt2;
+    plc.field("search_box") << search;
     
    
-   
-    plc.div("vertical <label margin=10>|70% <actions>");
-    plc["label"] << hello << btn;
-    plc["actions"] << act;
     plc.collocate();
     fm.show();
     exec();

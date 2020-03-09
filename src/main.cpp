77afcef5865ceaba4d8091d16660be1baeef9e14
TKF486 <60849963+TKF486@users.noreply.github.com>
2020-03-09 12:32:12 +0800

    set screen size
    size

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,13 @@
 #include <nana/gui/widgets/group.hpp>
 #include <nana/gui/widgets/menu.hpp>
 #include <nana/gui/widgets/widget.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include <nana/gui/widgets/menubar.hpp>
+#include <nana/gui/layout_utility.hpp>
+
+
+
+
 
 #include <forward_list>
 #include <map>
@@ -24,18 +31,17 @@
 using namespace nana;
 
 
-void on_menu_item(menu::item_proxy& ip)
-{
-    std::size_t index = ip.index();
-    std::cout << "Menu item index:" << index <<
-    std::endl;
-}
 
-int main()
+
+/*int main()
 {
        
     
-   form fm;
+   nana::appearance fmapper;
+   fmapper.maximize = true;
+   form fm(rectangle(0,0,1920,1080), fmapper);
+ 
+   
 
    
    fm.caption("Nana.exe");
@@ -43,27 +49,58 @@ int main()
     button bt1{fm,"Delete"};
      button bt2{fm, "Edit"};
      textbox search{fm};
-    
-    
+     label lb {fm};
+     lb.format(true);
+     lb.caption("<bold size = 30 font=\"Consolas\" >Book Managment</>");
+     lb.bgcolor(colors::azure);
+   lb.text_align(align::center, align_v::center);
+
+        
+     
+
 
     
-
+     
     
-
-			
-      
+    
     fm.show();
     place plc(fm);
-    plc.div("vertical <weight = 20% < vertical  <weight = 20% < buttons arrange = [100,100,100,100]>  <search_box margin = [0,0,0,300]>  > <> > <> > <> <>   ");
    
-    plc.field("buttons") << bt0 << bt1 << bt2;
-    plc.field("search_box") << search;
+    //plc.div("vertical <weight = 20% < vertical  <weight = 20% < buttons arrange = [100,100,100,100]>  <search_box margin = [0,0,0,300]>  > <> > <> > <> <>   ");
+    //plc.field("buttons") << bt0 << bt1 << bt2;
+    //plc.field("search_box") << search;
     
+    plc.div("vertical<weight = 10%  vertical<text>   ><>");
+    plc.field("text") << lb;
    
+    
+    
+    
+    
+    
     plc.collocate();
     fm.show();
     exec();
    
-    
+
+
+
+
+}
+*/
+
+int main(){
+    auto s = screen();
+    auto pa = s.get_primary().workarea();
+    form fm;
+
+    fm.move(pa.x,pa.y);
+    fm.outline_size({pa.width /2 , pa.height / 2});
+    fm.caption("Book Inventory");
+
+    fm.show();
+    exec();
+  
+
 
 }

9501f02b64e655ad1c74e7631f81f37e32896b49
TKF486 <60849963+TKF486@users.noreply.github.com>
2020-03-10 20:03:55 +0800

    program layout complete
    Layout:
    -header
    -buttons for search,
    add, delete, edit
    -listbox (table)

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@
 #include <nana/gui/widgets/label.hpp>
 #include <nana/gui/widgets/menubar.hpp>
 #include <nana/gui/layout_utility.hpp>
+#include <nana/gui/widgets/listbox.hpp>
+
+
+
 
 #include <cmath>
 #include <bitset>
@@ -42,7 +46,7 @@ int main()
     nana::appearance fmapper;
     fmapper.maximize = true;
     form fm(rectangle(0,0,1920,1080), fmapper);
- 
+    
    
 
    
@@ -98,16 +102,29 @@ int main(){
     textbox search_box{fm};
     label lb {fm};
     lb.format(true);
+    listbox table_box{fm, true};
+    table_box.append_header("ISBN Code", 80);
+    table_box.append_header("Author", 80);
+    table_box.append_header("Title", 80);
+    table_box.append_header("Publisher", 80);
+    table_box.append_header("Year Publish", 80);
+    table_box.append_header("Quantity", 80);
+    table_box.append_header("Price", 80);
+    table_box.append_header("Location", 80);    
 
     lb.caption("<bold size = 30 font=\"Consolas\" >Book Managment</>");
     lb.bgcolor(colors::azure);
     lb.text_align(align::center, align_v::center);
     place plc(fm);
-    plc.div("vertical<weight = 10%  vertical<header>   >      <weight = 20% <weight = 70%> <vertical <<weight = 70% <search_input  margin = [10,10,10,10] >  > <    <btn_search margin = [10,10,10,10]>  > >     < <weight = 70%> <    < btn_adv margin = [10,10,10,10] >     > > >  >            <>");
+    plc.div("vertical<weight = 10%  vertical<header> >  <weight = 20% <weight = 70%> <vertical <<weight = 70% <search_input  margin = [10,10,10,10] >  > <    <btn_search margin = [10,10,10,10]>  > >     < <weight = 60%> <    < btn_adv margin = [10,10,10,10]  >     > > >  >        < <weight =20%      vertical<weight = 70%     vertical<add margin = [20,20,20,20]><delete margin = [20,20,20,20]><edit margin = [20,20,20,20]>      > <weight = 30%>   >    <weight = 80% table_item> >");
     plc.field("header") << lb;
     plc.field("search_input") << search_box;
     plc.field("btn_search") << search_but;
     plc.field("btn_adv") << adv_search_but;
+    plc.field("add") << add_but;
+     plc.field("delete") << delete_but;
+      plc.field("edit") << edit_but;
+      plc.field("table_item") << table_box;
     
     
     plc.collocate();

95c4846f469b9ff24845edfbfeac52c372e1b0c7
TKF486 <60849963+TKF486@users.noreply.github.com>
2020-03-14 12:02:32 +0800

    Menu
    Simple menu made
    -with while loop
    -greetings
    -choice

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,101 @@
 #include <iostream>
 #include <iomanip>
 #include <conio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main() {
 
-    cout << "Hello Easy C++ project!" << endl;
+while (true)
+{
+    /* code */
 
-    cout << "Press any key to continue...";
-    getch();
-    return 0;
+
+//Greetings
+    cout <<"Hello Welcome to Book Inventory System!" << endl;
+    cout << "Check out what i can do by inputting the numbers below ^_^" << endl;
+    cout << "1) Add" << endl << "2) Delete" << endl << "3) Edit" << endl << "4) Search" << endl;
+    
+    //Choose
+    int user_input;
+    cout << "Input choice:";
+    cin >> user_input;
+    
+    //Add data
+    if (user_input == 1)
+    {
+         system("CLS");
+         cout << "Press any key to continue...";
+         getch();
+         system("CLS");
+    }
+
+    
+   
+    //Delete Data
+    else if (user_input == 2)
+    {
+         system("CLS");
+         cout << "Press any key to continue...";
+         getch();
+         system("CLS");
+    }
+
+    
+    //Edit Data
+    else if (user_input == 3)
+    {
+         system("CLS");
+         cout << "Press any key to continue...";
+         getch();
+         system("CLS");
+    }
+
+    
+    
+
+    //Search Data
+    else if (user_input == 4)
+    {
+         system("CLS");
+         cout << "Press any key to continue...";
+         getch();
+         system("CLS");
+    }
+    
+    //error checking (menu)
+    else
+    {
+        
+         system("CLS");
+         cout << "Error pls input value between 1 to 4";
+         getch();
+         system("CLS");
+    }
+    
+    
+   
+   
 }
+   
+   
+   
+  
+
+   
+   
+   }
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+  
+    
